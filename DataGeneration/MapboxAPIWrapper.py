65fe91de8342636e74e9fdab86a2fd9c9acd2756
mbramson <mathewbramson@gmail.com>
2015-11-28 00:27:14 -0500

    Extend functionality of MapboxAPIWrapper
    add ability to set origin/destination locations
    
    add method to construct a request string with the locations and api key
    
    begun methods for getting the distance through the api call

--- DataGeneration/MapboxAPIWrapper.py
+++ DataGeneration/MapboxAPIWrapper.py
@@ -1,4 +1,5 @@
 from urllib2 import Request, urlopen, URLError
+from MapLocation import MapLocation
 import os
 
 class MapboxAPIWrapper:
@@ -10,3 +11,39 @@ class MapboxAPIWrapper:
             raise ValueError("Could not find an api key file")
         with open(filename) as key_file:
             self.key = key_file.read()
+
+    def set_origin_location(self, origin):
+        if not isinstance(origin, MapLocation):
+            raise TypeError('Origin Location must be a MapLocation object')
+        self.origin = origin
+
+    def set_destination_location(self, destination):
+        if not isinstance(destination, MapLocation):
+            raise TypeError('Destination Location must be a MapLocation object')
+        self.destination = destination
+
+    def construct_request_string(self):
+        request_string = 'https://api.mapbox.com/v4/directions/mapbox.walking/'
+        if not hasattr(self, 'origin'):
+            raise UnboundLocalError('origin has not been specified')
+        if not hasattr(self, 'destination'):
+            raise UnboundLocalError('destination has not been specified')
+        if not hasattr(self, 'key'):
+            raise UnboundLocalError('key has not been specified')
+        request_string += str(self.origin.latitude) + ','
+        request_string += str(self.origin.longitude) + ';'
+        request_string += str(self.destination.latitude) + ','
+        request_string += str(self.destination.longitude)
+        request_string += ('.json?alternatives=false&instructions=text&geometry='
+                           'false&steps=false&&access_token=')
+        request_string += self.key
+        return request_string
+
+    def get_distance_from_api(self):
+        request_string = self.construct_request_string()
+        pass
+
+    def make_api_call(self, request_string):
+        request = Request(request_string)
+        response = urlopen(request)
+        return response.read()
--- DataGeneration/test_MapboxAPIWrapper.py
+++ DataGeneration/test_MapboxAPIWrapper.py
@@ -1,6 +1,7 @@
 import unittest, mock
 from mock import patch, mock_open
 from MapboxAPIWrapper import MapboxAPIWrapper
+from MapLocation import MapLocation
 
 from sys import version_info
 if version_info.major == 2:
@@ -12,8 +13,9 @@ class TestMapboxAPIWrapper(unittest.TestCase):
 
     def test_mapbox_api_wrapper_class_exists(self):
         wrapper = MapboxAPIWrapper()
-        self.assertTrue(True)
+        self.assertIsInstance(wrapper, MapboxAPIWrapper)
 
+    # load_api_key_from_file tests
     @patch('MapboxAPIWrapper.os.path')
     def test_mapbox_load_api_key_checks_for_file_existance(self, mock_os_path):
         wrapper = MapboxAPIWrapper()
@@ -36,4 +38,83 @@ class TestMapboxAPIWrapper(unittest.TestCase):
             wrapper.load_api_key_from_file(filename='abc.txt')
         self.assertEquals('api_key', wrapper.key)
 
+    # set_origin_location tests
+    def test_mapbox_set_origin_location_is_MapLocation(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.set_origin_location(MapLocation())
+        self.assertIsInstance(wrapper.origin, MapLocation)
 
+    def test_mapbox_set_origin_location_errors_for_wrong_type(self):
+        wrapper = MapboxAPIWrapper()
+        self.assertRaises(TypeError, wrapper.set_origin_location, "string")
+
+    # set_destination_location tests
+    def test_mapbox_set_destination_location_is_MapLocation(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.set_destination_location(MapLocation())
+        self.assertIsInstance(wrapper.destination, MapLocation)
+
+    def test_mapbox_set_destination_location_errors_for_wrong_type(self):
+        wrapper = MapboxAPIWrapper()
+        self.assertRaises(TypeError, wrapper.set_destination_location, "string")
+
+    # construct_request_string tests
+    def test_construct_request_string_returns_string(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.origin = MapLocation()
+        wrapper.destination = MapLocation()
+        wrapper.key = 'api_key'
+        self.assertIsInstance(wrapper.construct_request_string(), str)
+
+    def test_construct_request_string_errors_if_no_origin(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.destination = MapLocation()
+        wrapper.key = 'api_key'
+        self.assertRaises(UnboundLocalError, wrapper.construct_request_string)
+
+    def test_construct_request_string_errors_if_no_destination(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.origin = MapLocation()
+        wrapper.key = 'api_key'
+        self.assertRaises(UnboundLocalError, wrapper.construct_request_string)
+
+    def test_constuct_request_string_errors_if_no_key(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.origin = MapLocation()
+        wrapper.destination = MapLocation()
+        self.assertRaises(UnboundLocalError, wrapper.construct_request_string)
+
+    def test_construct_request_string_produces_correct_output(self):
+        wrapper = MapboxAPIWrapper()
+        wrapper.origin = MapLocation(latitude = 50.032,
+                                     longitude = 40.54453)
+        wrapper.destination = MapLocation(latitude = 51.0345,
+                                          longitude = 41.2314)
+        wrapper.key = 'api_key'
+        self.assertEqual('https://api.mapbox.com/v4/directions/mapbox.walking/'
+                         '50.032,40.54453;51.0345,41.2314.json?alternatives='
+                         'false&instructions=text&geometry=false&steps=false&&'
+                         'access_token=api_key',
+                         wrapper.construct_request_string())
+
+    # get_dstiance_from_api tests
+    @patch('MapboxAPIWrapper.MapboxAPIWrapper.construct_request_string')
+    def test_get_distance_from_api_constructs_request_string(self,
+                                                             mock_construct):
+        mock_construct.return_value = 'api_request'
+        wrapper = MapboxAPIWrapper()
+        wrapper.get_distance_from_api()
+        self.assertTrue(mock_construct.called, 'api call must construct '
+                                                    'request string')
+
+    # make_api_call tests
+    #def test_make_api_call_returns_string(self):
+    #    wrapper = MapboxAPIWrapper()
+    #    self.assertIsInstance(wrapper.make_api_call('request_string'), tuple)
+
+    @patch('MapboxAPIWrapper.urlopen')
+    def test_make_api_call_calls_urlopen(self, mock_urlopen):
+        mock_urlopen.return_value = 'response_string'
+        wrapper = MapboxAPIWrapper()
+        wrapper.make_api_call('request_string')
+        self.assertTrue(mock_urlopen.called, 'make_api_call must call urlopen')
