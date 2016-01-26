56f4a04757cfe3d99b33765e1acc2f5ec985fb10
mbramson <mathewbramson@gmail.com>
2016-01-25 23:29:00 -0500

    add link to DatabaseHandler from DataGenerator

--- DataGeneration/DataGenerator.py
+++ DataGeneration/DataGenerator.py
@@ -6,3 +6,10 @@ class DataGenerator:
 
     def __init__(self):
         pass
+
+    def start(self):
+        handler = self.get_database_handler()
+
+    def get_database_handler(self, db_file_name='db.sqlite3'):
+        handler = DatabaseHandler(db_file_name)
+        return handler
--- DataGeneration/tests/test_DataGenerator.py
+++ DataGeneration/tests/test_DataGenerator.py
@@ -1,9 +1,22 @@
 from DataGeneration.DataGenerator import DataGenerator
+from DataGeneration.DatabaseHandler import DatabaseHandler
 import unittest
-
+from mock import Mock
 
 class test_DataGenerator(unittest.TestCase):
 
     def test_DataGenerator_exists(self):
         generator = DataGenerator()
         self.assertIsInstance(generator, DataGenerator)
+
+    def test_begin_initializes_database(self):
+        generator = DataGenerator()
+        mock_get_database_handler = Mock()
+        generator.get_database_handler = mock_get_database_handler
+        generator.start()
+        mock_get_database_handler.assert_called_once_with()
+
+    # get_database_handler tests
+    def test_get_database_handler_returns_DatabaseHandler(self):
+        generator = DataGenerator()
+        self.assertIsInstance(generator.get_database_handler(), DatabaseHandler)
