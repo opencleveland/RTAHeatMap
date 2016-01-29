71ed98da0965d6d8283286bc73175f6e389e89d2
mbramson <mathewbramson@gmail.com>
2016-01-28 22:35:39 -0500

    add patch mocks to test_DataGenerator

--- DataGeneration/tests/test_DataGenerator.py
+++ DataGeneration/tests/test_DataGenerator.py
@@ -1,20 +1,22 @@
-from DataGeneration.DataGenerator import DataGenerator
-from DataGeneration.DatabaseHandler import DatabaseHandler
+from DataGeneration import DataGenerator
+from DataGeneration import DatabaseHandler
 import unittest
-from mock import Mock
+from mock import Mock, patch
 
 class test_DataGenerator(unittest.TestCase):
 
-    def test_DataGenerator_exists(self):
+    @patch('DataGeneration.DataGenerator.get_database_handler')
+    def test_DataGenerator_exists(self,
+                                  mock_get_db):
         generator = DataGenerator()
         self.assertIsInstance(generator, DataGenerator)
 
-    def test_begin_initializes_database(self):
+    @patch('DataGeneration.DataGenerator.get_database_handler')
+    def test_begin_initializes_database(self,
+                                        mock_get_db):
         generator = DataGenerator()
-        mock_get_database_handler = Mock()
-        generator.get_database_handler = mock_get_database_handler
         generator.start()
-        mock_get_database_handler.assert_called_once_with()
+        mock_get_db.assert_called_once_with()
 
     # get_database_handler tests
     def test_get_database_handler_returns_DatabaseHandler(self):
