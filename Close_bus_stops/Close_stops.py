50b1c904cadbb344d606dd8e214ebb472070f9ca
Takuya-Sakaguchi <takuya.sakaguchi@gmail.com>
2016-01-31 11:39:09 -0500

    add new function and delete csv

--- Close_bus_stops/Close_stops.py
+++ Close_bus_stops/Close_stops.py
@@ -1,8 +1,16 @@
 
+
+'''
+Requirements:
+pandas
+geopy
+'''
+
 import pandas as pd
+from geopy.distance import vincenty
 
 
-def datafame_of_close_stops(given_lat, given_lon, df, given_range=0.005):
+def dataframe_of_close_stops(given_lat, given_lon, df, given_range=0.007225):
     selectedDf = df[(df.stop_lat > (given_lat - given_range)) \
                     & (df.stop_lat < (given_lat + given_range))\
                     &(df.stop_lon > (given_lon - given_range)) \
@@ -10,19 +18,34 @@ def datafame_of_close_stops(given_lat, given_lon, df, given_range=0.005):
     return selectedDf
 
 
+def get_number_of_close_stops(given_lat, given_lon, df, given_range=0.007225):
+    number = len(dataframe_of_close_stops(given_lat, given_lon, df, given_range))
+    return number
+
+
+def Add_distance_to_dataframe(addr_df, destination_lat, destination_lon):
+    addr_df.index = range(0, len(addr_df))
+    for i in range(len(addr_df)):
+        addr_df.loc[i, "distance"] = vincenty((destination_lat, destination_lon), \
+                                              (addr_df.loc[i, "stop_lat"], addr_df.loc[i,"stop_lon"])).miles
+    return addr_df
+
+
 def main():
 
     stopCSVfileName = "/Users/TakuyaSakaguchi/Jupyter_Python3/stops.csv"
-    df = pd.read_csv(stopCSVfileName)
+    stop_df = pd.read_csv(stopCSVfileName)
 
-    lat = 41.392864
+    lat = 41.592864
     lon = -81.536557
 
-    selectedStopsDf = datafame_of_close_stops(lat, lon, df)
+    selectedStopsDf = dataframe_of_close_stops(lat, lon, stop_df)
+
+
 
     print(selectedStopsDf.head())
     print("\n")
-    print(len(selectedStopsDf))
+    print(get_number_of_close_stops(lat, lon, stop_df))
 
 if __name__ == "__main__":
     main()
