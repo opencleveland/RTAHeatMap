b95ccd0c9c752bd306f10765b7025186050b5e75
mbramson <mathewbramson@gmail.com>
2016-02-27 22:16:18 -0500

    add docstrings to DataGenerator

--- DataGeneration/DataGenerator.py
+++ DataGeneration/DataGenerator.py
@@ -12,11 +12,29 @@ class DataGenerator:
         self.wrapper = MapboxAPIWrapper()
 
     def initialize(self, db='db.sqlite3', api_key='api_key.txt'):
+        """
+        Initializes the DataGenerator. Connects to the database, collects all
+        stops from the database, and initializes the API wrapper.
+
+        Args:
+        db (str): File path to the sqlite3 database.
+        api_key (str): File path to a text file containing an API key.
+        """
         self.handler = self._get_database_handler(db)
         self.stops = self.handler.get_all_stops()
         self.wrapper = self._get_api_wrapper(api_key)
 
     def begin(self, stops_per_address=5):
+        """
+        Begins collection of distances to closest stops from each address.
+        Stores each address-stop pair and associated walking distance and time
+        in the routes table of the database.
+
+        Args:
+        stops_per_address (int): Number of stops per address used to query the
+            api for walking distance. The stops selected are the closest stops
+            to the address by straight line distance. Default value is 5.
+        """
         address_generator = self.handler.get_address_generator()
         for address in address_generator:
             closest_stops = self._get_closest_locations(address,
