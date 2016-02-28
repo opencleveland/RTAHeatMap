a8a07dd341e3b20456939f7de3e84474d8f1ad09
mbramson <mathewbramson@gmail.com>
2016-02-27 21:15:38 -0500

    refactor DataGenerator begin tests

--- DataGeneration/tests/test_DataGenerator.py
+++ DataGeneration/tests/test_DataGenerator.py
@@ -3,7 +3,7 @@ from DataGeneration import DatabaseHandler
 from DataGeneration import MapLocation
 from DataGeneration import MapboxAPIWrapper
 import unittest
-from mock import Mock, patch
+from mock import Mock, patch, MagicMock
 
 
 class test_DataGenerator(unittest.TestCase):
@@ -85,66 +85,52 @@ class test_DataGenerator(unittest.TestCase):
         mock_get_key.assert_called_once_with('api_key.txt')
 
     # begin tests
-    @patch('DataGeneration.DatabaseHandler.get_all_stops')
-    @patch('DataGeneration.DatabaseHandler.get_address_without_route_generator')
-    def test_begin_calls_handler_get_next_address(self,
-                                                  mock_get_address,
-                                                  mock_get_all_stops):
-        generator = DataGenerator()
-        generator.begin()
+    def test_begin_calls_handler_get_next_address(self):
+        self.generator.handler.get_address_without_route_generator = MagicMock()
+        self.generator.handler.get_all_stops = Mock()
 
-        mock_get_address.assert_called_once_with()
+        self.generator.begin()
+        self.generator.handler.get_address_without_route_generator.\
+            assert_called_once_with()
 
-    @patch('DataGeneration.DatabaseHandler.get_all_stops')
-    @patch('DataGeneration.DatabaseHandler.get_address_without_route_generator')
-    def test_begin_calls_get_all_stops(self,
-                                       mock_get_address,
-                                       mock_get_all_stops):
-        generator = DataGenerator()
-        generator.begin()
-        mock_get_all_stops.assert_called_once_with()
+    def test_begin_calls_get_all_stops(self):
+        self.generator.handler.get_address_without_route_generator = MagicMock()
+        self.generator.handler.get_all_stops = Mock()
 
-    @patch('DataGeneration.DatabaseHandler.get_all_stops')
-    @patch('DataGeneration.DatabaseHandler.get_address_without_route_generator')
-    def test_begin_calls_get_closest_locations(self,
-                                               mock_get_address,
-                                               mock_get_all_stops):
-        generator = DataGenerator()
+        self.generator.begin()
+        self.generator.handler.get_all_stops.assert_called_once_with()
 
-        address = MapLocation(1, 1, 1)
-        mock_get_address.return_value = [address]  # List to emulate generator
+    def test_begin_calls_get_closest_locations(self):
+        addresses = MapLocation(1, 1, 1)
+        self.generator.handler.get_address_without_route_generator = \
+            MagicMock(return_value=[addresses])
 
         stops = [MapLocation(2, 2, 2), MapLocation(3, 3, 3)]
-        mock_get_all_stops.return_value = stops
+        self.generator.handler.get_all_stops = Mock(return_value=stops)
 
         mock_get_closest_locations = Mock(return_value=[stops[0]])
-        generator.get_closest_locations = mock_get_closest_locations
+        self.generator.get_closest_locations = mock_get_closest_locations
 
-        generator.wrapper.get_distance_from_api = Mock()
+        self.generator.wrapper.get_distance_from_api = Mock()
 
-        generator.begin(stops_to_query=1)
-        mock_get_closest_locations.assert_called_once_with(address, stops, n=1)
+        self.generator.begin(stops_to_query=1)
+        mock_get_closest_locations.assert_called_once_with(addresses, stops, n=1)
 
-    @patch('DataGeneration.DatabaseHandler.get_all_stops')
-    @patch('DataGeneration.DatabaseHandler.get_address_without_route_generator')
-    def test_begin_calls_wrapper_get_distance_from_api(self,
-                                                       mock_get_address,
-                                                       mock_get_all_stops):
-        generator = DataGenerator()
-
-        address = MapLocation(1, 1, 1)
-        mock_get_address.return_value = [address]  # List to emulate generator
+    def test_begin_calls_wrapper_get_distance_from_api(self):
+        addresses = MapLocation(1, 1, 1)
+        self.generator.handler.get_address_without_route_generator = \
+            MagicMock(return_value=[addresses])
 
         stops = [MapLocation(2, 2, 2), MapLocation(3, 3, 3)]
-        mock_get_all_stops.return_value = stops
+        self.generator.handler.get_all_stops = Mock(return_value=stops)
 
-        generator.get_closest_locations = Mock(return_value=[stops[0]])
+        self.generator.get_closest_locations = Mock(return_value=[stops[0]])
 
         mock_get_distance = Mock()
-        generator.wrapper.get_distance_from_api = mock_get_distance
+        self.generator.wrapper.get_distance_from_api = mock_get_distance
 
-        generator.begin(stops_to_query=1)
-        mock_get_distance.assert_called_once_with(address, stops[0])
+        self.generator.begin(stops_to_query=1)
+        mock_get_distance.assert_called_once_with(addresses, stops[0])
 
     # get_closest_locations tests
     def test_get_closest_locations_returns_closest_single_location_1(self):
